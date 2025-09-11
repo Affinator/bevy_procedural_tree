16101756fda2d77702fa5f68d3db31e2403f2592
Affinator <hendrik@decke.online>
2025-09-11 10:30:13 +0200

    typo

--- src/errors.rs
+++ src/errors.rs
@@ -7,6 +7,6 @@ impl Error for IndicesOverflowError {}
 
 impl Display for IndicesOverflowError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Indices overflow in mesh generation: Please reduce amount of sections, segments or leaves or enable the indices_u32 feature.")
+        write!(f, "Indices overflow in mesh generation: Please reduce amount of sections, segments or leaves or enable the u32_indices feature.")
     }
 }
